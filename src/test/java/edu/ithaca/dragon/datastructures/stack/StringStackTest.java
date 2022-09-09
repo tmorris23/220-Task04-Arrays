fcded060934952fdc43ab68c70524c38052e01a1
Talia Morris <tmorris@ithaca.edu>
2022-09-09 11:31:22 -0400

    added comments

--- src/test/java/edu/ithaca/dragon/datastructures/stack/StringStackTest.java
+++ src/test/java/edu/ithaca/dragon/datastructures/stack/StringStackTest.java
@@ -20,25 +20,25 @@ public class StringStackTest {
     }
 
     public void pushPopIsEmptyTest(StringStack myStack, int stackSize){
-        assertTrue(myStack.isEmpty());
+        assertTrue(myStack.isEmpty()); //ensuring that the stack just created is empty
         
-        for (int i=0; i<stackSize; i++){
+        for (int i=0; i<stackSize; i++){ //using a for loop to push values into the stack
             myStack.push(Integer.toString(i));
             assertFalse(myStack.isEmpty());
         }
-        for (int i=stackSize-1; i>=0; i--){
+        for (int i=stackSize-1; i>=0; i--){ //using a for loop to remove values from the stack from right to left
             assertFalse(myStack.isEmpty());
             assertEquals(Integer.toString(i), myStack.pop());
         }
-        assertTrue(myStack.isEmpty());
+        assertTrue(myStack.isEmpty()); // stack should be empty
 
-        myStack.push("a");
+        myStack.push("a"); // pushes more items into the stack 
         myStack.push("b");
         myStack.push("c");
-        assertFalse(myStack.isEmpty());
+        assertFalse(myStack.isEmpty()); //stack shouldn't be empty here
         assertEquals("c", myStack.pop());
         assertEquals("b", myStack.pop());
-        assertFalse(myStack.isEmpty());
+        assertFalse(myStack.isEmpty()); //a should still be in the stack 
         myStack.push("x");
         myStack.push("y");
         assertFalse(myStack.isEmpty());
@@ -48,16 +48,16 @@ public class StringStackTest {
         assertTrue(myStack.isEmpty());
     }
 
-    public void testPopOnEmpty(StringStack emptyStack){
+    public void testPopOnEmpty(StringStack emptyStack){ //testing is NoSuchElementException happens when popping on an empty stack
         assertTrue(emptyStack.isEmpty());
         assertThrows(NoSuchElementException.class, () -> emptyStack.pop());
     }
 
-    public void testMakeEmpty(StringStack myStack, int stackSize){
-        assertTrue(myStack.isEmpty());
+    public void testMakeEmpty(StringStack myStack, int stackSize){ //testing if empty
+        assertTrue(myStack.isEmpty());//calling ifEmpty on an empty stack
         for (int i=0; i<stackSize; i++){
             myStack.push(Integer.toString(i));
-            assertFalse(myStack.isEmpty());
+            assertFalse(myStack.isEmpty());//calling ifEmpty on a populated stack
         }
         myStack.makeEmpty();
         testPopOnEmpty(myStack);
