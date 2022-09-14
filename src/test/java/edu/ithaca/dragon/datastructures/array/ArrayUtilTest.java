82e17106bf66b37e4e3a0810c720d52f692d1198
Talia Morris <tmorris@ithaca.edu>
2022-09-14 16:51:43 -0400

    Made changes to test

--- src/test/java/edu/ithaca/dragon/datastructures/array/ArrayUtilTest.java
+++ src/test/java/edu/ithaca/dragon/datastructures/array/ArrayUtilTest.java
@@ -1,26 +1,29 @@
 package edu.ithaca.dragon.datastructures.array;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import edu.ithaca.dragon.datastructures.array.ArrayUtil;
 import edu.ithaca.dragon.datastructures.stack.ArrayStringStack;
 
 public class ArrayUtilTest {
 
     int numItemsToTestWith = 5;
-    ArrayStringStack myArray = new ArrayStringStack();
-    ArrayStringStack doubleStack =createDoubleSizeArrayTest (myArray, numItemsToTestWith);
+    String[] myArray = {
+        "0","1","2","3","4"
+    };
 
     @Test
-    public ArrayStringStack createDoubleSizeArrayTest(ArrayStringStack myStack, int stackSize){
-        assertTrue(myStack.isEmpty()); //ensuring that the stack just created is empty
+    public void createDoubleSizeArrayTest( String[] myStack, int stackSize){
+        assertEquals(5,myStack.length); //ensuring that the stack just created is empty
+
         ArrayStringStack doubleArray = new ArrayStringStack();
-        doubleArray = createDoubleSizeArray(myStack);
+        ArrayUtil.createDoubleSizeArray(myStack);
         
         assertFalse(doubleArray.isEmpty());
-        assertEquals(10,doubleArray.size);
+        assertEquals(10,doubleArray.getSize());
         
     }
     
